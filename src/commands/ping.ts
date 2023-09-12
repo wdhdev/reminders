479489fa8223aee448bcaa1cf7f3a922bd34d11e
William Harrison <william@williamdavidharrison.com.au>
2023-09-12 16:14:24 +0800

    feat: ping command

---
+++ src/commands/ping.ts
@@ -0,0 +1,52 @@
+import Command from "../classes/Command";
+import ExtendedClient from "../classes/ExtendedClient";
+import { Message } from "discord.js";
+
+import { emojis as emoji } from "../config";
+
+const command: Command = {
+    name: "ping",
+    description: "Get the bot's latency.",
+    aliases: ["latency"],
+    botPermissions: [],
+    cooldown: 5,
+    enabled: true,
+    async execute(message: Message, args: string[], cmd: Command, client: ExtendedClient & any, Discord: any) {
+        try {
+            const botLatency = Date.now() - message.createdTimestamp;
+            const apiLatency = Math.round(client.ws.ping);
+
+            let botLatencyValue;
+            let apiLatencyValue;
+
+            if(botLatency >= 0 && botLatency <= 99) {
+                botLatencyValue = `${emoji.connection_excellent} ${botLatency}ms`;
+            } else if(botLatency >= 100 && botLatency <= 199) {
+                botLatencyValue = `${emoji.connection_good} ${botLatency}ms`;
+            } else {
+                botLatencyValue = `${emoji.connection_bad} ${botLatency}ms`;
+            }
+
+            if(apiLatency >= 0 && apiLatency <= 99) {
+                apiLatencyValue = `${emoji.connection_excellent} ${apiLatency}ms`;
+            } else if(apiLatency >= 100 && apiLatency <= 199) {
+                apiLatencyValue = `${emoji.connection_good} ${apiLatency}ms`;
+            } else {
+                apiLatencyValue = `${emoji.connection_bad} ${apiLatency}ms`;
+            }
+
+            const ping = new Discord.EmbedBuilder()
+                .setColor(client.config_embeds.default)
+                .addFields (
+                    { name: "Bot Latency", value: botLatencyValue, inline: true },
+                    { name: "API Latency", value: apiLatencyValue, inline: true }
+                )
+
+            message.reply({ embeds: [ping] });
+        } catch(err) {
+            client.logCommandError(err, message, Discord);
+        }
+    }
+}
+
+export = command;
--- src/config.ts
+++ src/config.ts
@@ -6,6 +6,9 @@ const embeds = {
 }
 
 const emojis = {
+    connection_bad: "<:connection_bad:1149583879179612250>",
+    connection_excellent: "<:connection_excellent:1149583873538260992>",
+    connection_good: "<:connection_good:1149583875551539280>",
     cross: "<:cross:1149583869956329492>",
     reply: "<:reply:1149583863333519390>",
     tick: "<:tick:1149583861416730695>"
