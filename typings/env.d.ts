b6f38913a7e49435854b871d4944d7455838b6ee
William Harrison <william@williamdavidharrison.com.au>
2024-01-15 13:18:52 +0800

    Update env.d.ts

--- typings/env.d.ts
+++ typings/env.d.ts
@@ -4,6 +4,7 @@ declare global {
     namespace NodeJS {
         interface ProcessEnv {
             clientId: Snowflake;
+            database: string;
             sentry_dsn: string;
             token: string;
         }
