c7d049cd36b4233aed8c4b5674bb92088b2b9c05
William Harrison <87287585+wdhdev@users.noreply.github.com>
2024-12-03 11:43:24 +0800

    Update mongo.ts

--- src/lib/mongo.ts
+++ src/lib/mongo.ts
@@ -4,12 +4,15 @@ import * as Sentry from "@sentry/node";
 require("dotenv").config();
 
 export default async () => {
-    return mongoose.connect(process.env.MONGO_URI).then(() => {
-        console.log("Connected to Database!");
-    }).catch((err: Error) => {
-        Sentry.captureException(err);
-        console.error(err);
+    return mongoose
+        .connect(process.env.MONGO_URI, { dbName: process.env.DEVELOPMENT ? "reminders-dev" : "reminders-prod" })
+        .then(() => {
+            console.log("Connected to Database!");
+        })
+        .catch((err: Error) => {
+            Sentry.captureException(err);
+            console.error(err);
 
-        process.exit(1);
-    })
-}
+            process.exit(1);
+        });
+};
